0e3faf75b709caecabf24d0259c62f69288e666d
Ludrick <luciano.oliveira@belgobekaert.com.br>
2021-03-02 01:57:17 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import streamlit as st
+
+st.title("Esse eh o titulo!")
+
+st.markdown("## Markdown com 2 sustenidos")
+st.markdown("### Markdown com 3 sustenidos")
+
+st.success("Texto de sucesso!!!")
